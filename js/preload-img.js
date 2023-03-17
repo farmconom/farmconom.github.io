cc8c8517a028062cb1fc28f19c1c77bcde5ae9a5
farmconom <farmconom@gmail.com>
2023-03-17 23:26:52 +0700

    fix preload2

--- js/preload-img.js
+++ js/preload-img.js
@@ -1,21 +1,20 @@
-function preload_image(img_url) {
-    let img = new Image();
-    img.src = img_url;
-  }
-  
-  preload_image("");
+const images = ['../images/agency-img.jpg',
+                '../images/bnw-img.jpg',
+                '../images/dropbeat-img.jpg',
+                '../images/icon-PL.png',
+                '../images/logo-PL.png',
+                '../images/poster-video.jpg',
+                '../images/profile-art.jpg',
+                '../images/profile.jpg',
+                '../images/resume.jpg',
+                '../images/twitter-card.jpg'];
 
-  preload_image("../images/agency-img.jpg"),
-               ("../images/bnw-img.jpg"),
-               ("../images/dropbeat-img.jpg"),
-               ("../images/icon-PL.png"),
-               ("../images/logo-PL.png"),
-               ("../images/poster-video.jpg"),
-               ("../images/profile-art.jpg"),
-               ("../images/profile.jpg"),
-               ("../images/resume.jpg"),
-               ("../images/twitter-card.jpg");
+images.forEach((image) => {
+  const img = new Image();
+  img.src = image;
+});
 
+//preload video
 const video = document.createElement('video');
 video.src = '../video/background-video.mov';
 video.preload = 'auto';
